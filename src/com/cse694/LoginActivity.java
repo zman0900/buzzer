e7aa8c794cff15e275083cdb9eaf64ea5f47bc2f
Dan Ziemba <zman0900@gmail.com>
2012-05-02 16:40:42 -0400

    Added cancel button

--- src/com/cse694/LoginActivity.java
+++ src/com/cse694/LoginActivity.java
@@ -3,12 +3,27 @@ package com.cse694;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 		Log.d("Buzzer","Showing login view");
+		
+		View btnCancel=(Button)findViewById(R.id.cancel_button);
+        btnCancel.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+  		case R.id.cancel_button:
+	    	finish();
+    		break;
+		}
 	}
 }
