0d0644dffd4c69b3e85bfb5a0f385aa7b3eba500
Dan Ziemba <zman0900@gmail.com>
2012-05-02 21:20:49 -0400

    Enabled registration, login, and logout

--- src/com/cse694/BuzzerActivity.java
+++ src/com/cse694/BuzzerActivity.java
@@ -3,16 +3,35 @@ package com.cse694;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class BuzzerActivity extends Activity {
+public class BuzzerActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        View btnLogout = (Button)findViewById(R.id.logoutButton);
+        btnLogout.setOnClickListener(this);
+        
         // Check for login
-        // if (!logged in) {
-        	startActivity(new Intent("com.cse694.LoginActivity"));
+        if (!LoginActivity.isLoggedIn(this)) {
+        	finish();
+        	startActivity(new Intent(BuzzerActivity.this,LoginActivity.class));
+        }
     }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.logoutButton:
+			LoginActivity.logout(this);
+			finish();
+			startActivity(new Intent(BuzzerActivity.this,LoginActivity.class));
+			break;
+		}
+	}
 }
---
+++ src/com/cse694/LoginDatabaseHelper.java
@@ -0,0 +1,54 @@
+package com.cse694;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LoginDatabaseHelper extends SQLiteOpenHelper {
+	
+	private static final String DATABASE_NAME = "BuzzerAccounts.db";
+	private static final int DATABASE_VERSION = 1;
+	private static final String TABLE_NAME = "Accounts";
+	private static final String COL_EMAIL = "Email";
+	private static final String COL_PNAME = "PartyName";
+	private static final String COL_PASS = "Password";
+
+	public LoginDatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE "+TABLE_NAME+" ("+COL_EMAIL+" VARCHAR(255) PRIMARY KEY, "+COL_PNAME+" VARCHAR(255), "+COL_PASS+" VARCHAR(255))");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+		onCreate(db);
+	}
+
+	public void insertUser(String email, String partyName, String password) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		cv.put(COL_EMAIL, email);
+		cv.put(COL_PNAME, partyName);
+		cv.put(COL_PASS, password);
+		db.insert(TABLE_NAME, null, cv);
+		db.close();
+	}
+	
+	public Boolean checkUser(String email, String password) {
+		Boolean result = false;
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.query(TABLE_NAME, null, COL_EMAIL+"=? AND "+COL_PASS+"=?", new String[] {email,password}, null, null, null);
+		if(c.moveToFirst()) {
+			// Got a result so login is correct
+			result = true;
+		}
+		db.close();
+		return result;
+	}
+}
