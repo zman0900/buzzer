1da025c5a70aee2481d7c424e2b472476302a929
Alan Weide <alan.d.weide@gmail.com>
2012-06-02 18:33:50 -0400

    Latest (small) updates

---
+++ src/com/cse694/UserAcceptsSeat.java
@@ -0,0 +1,15 @@
+package com.cse694;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class UserAcceptsSeat extends Activity implements OnClickListener {
+
+	@Override
+	public void onClick(View v) {
+		
+	}
+	
+
+}
